17fc2f7f0cfeacc65043a84aa8285de7be2ef9f6
Joseph balikuddembe <baliksjosay@gmail.com>
2020-04-22 22:48:18 +0300

    Add image upload

--- api/models.py
+++ api/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=50)
+    cover = models.ImageField(upload_to='images/')
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,7 @@
-from django.urls import path
+from django.contrib import admin
+from django.conf import settings # new
+from django.urls import path, include # new
+from django.conf.urls.static import static # new
 
 from . import views
 
@@ -6,3 +9,6 @@ urlpatterns = [
     path('', views.PostList.as_view()),
     path('<int:pk>/', views.PostDetail.as_view()),
 ]
+
+if settings.DEBUG: # new
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
